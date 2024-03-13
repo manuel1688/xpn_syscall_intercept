6e38c207991b57ba01dda4514928d135e7c48733
Manuel Otero <manuelotero@Manuels-MacBook-Pro.local>
2024-03-13 12:34:47 +0100

    ajuste de resturn

--- xpn_syscall_intercept_create.c
+++ xpn_syscall_intercept_create.c
@@ -311,8 +311,10 @@ hook(long syscall_number,
 
 			fd  = xpn_creat((const char *)skip_xpn_prefix(path),mode);
 			ret = add_xpn_file_to_fdstable(fd);
-      printf("The value of my_var is: %d\n", ret);
+      printf("ret: %d\n", ret);
 			debug_info("[bypass]\t creat %s -> %d", skip_xpn_prefix(path), ret);
+      printf("fd: %d\n", fd);
+      *result = ret;
       return ret;
 		}
 		// Not an XPN partition. We must link with the standard library
