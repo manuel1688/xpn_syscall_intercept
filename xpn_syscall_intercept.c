cfa43397412e8e6843e362075eaccad37445f4fb
Manuel Otero <manuelotero@Manuels-MacBook-Pro.local>
2024-04-24 11:39:37 +0200

    ajuste

--- xpn_syscall_intercept.c
+++ xpn_syscall_intercept.c
@@ -231,7 +231,7 @@ static int hook(long syscall_number,long arg0, long arg1,long arg2, long arg3,lo
   else if(syscall_number == SYS_newfstatat)
   {
     // (AT_FDCWD, "/tmp/expand/P1/demo.txt", 0x7ffd6c137000, 0x0)
-    int fd = (int)arg0;
+    // int fd = (int)arg0;
     char *path = (char *)arg1;
     struct stat *buf = (struct stat *)arg2;
     
