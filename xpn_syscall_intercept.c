ab8e6202c401f1d7a8cc73098a70e327e3bb8528
Manuel Otero <manuelotero@Manuels-MacBook-Pro.local>
2024-04-25 11:18:38 +0200

    fstat

--- xpn_syscall_intercept.c
+++ xpn_syscall_intercept.c
@@ -235,8 +235,9 @@ static int hook(long syscall_number,long arg0, long arg1,long arg2, long arg3,lo
     // (AT_FDCWD, "/tmp/expand/P1/demo.txt", 0x7ffd6c137000, 0x0)
     int fd = (int)arg0;
     char *path = (char *)arg1;
+    struct stat *buf = (struct stat *)arg2;
     int ret = -1;
-  
+
     if (fd != -100){
       
       struct generic_fd virtual_fd = fdstable_get(fd);
@@ -253,7 +254,6 @@ static int hook(long syscall_number,long arg0, long arg1,long arg2, long arg3,lo
     
     } else if (is_xpn_prefix(path))
     {
-      struct stat *buf = (struct stat *)arg2;
       xpn_adaptor_keepInit();
       ret = xpn_stat(skip_xpn_prefix(path), buf);
       *result = ret;
