5fc4ab8c31088ccb5d6337ff68a94f333872e4ea
Manuel Otero <manuelotero@Manuels-MacBook-Pro.local>
2024-05-06 09:52:10 +0200

    ajuste

--- xpn_syscall_intercept.c
+++ xpn_syscall_intercept.c
@@ -333,6 +333,7 @@ static int hook(long syscall_number,long arg0, long arg1,long arg2, long arg3,lo
   }
   else if (syscall_number == SYS_access)
   {
+    char *path = (char *)arg0;
     struct stat64 stats;
     int ret = -1;
     if (is_xpn_prefix(path))
